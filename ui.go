319c01aa118b9f795a19b11274a446bd5005bb4b
cantclosevim <cantclosevim@gmail.com>
2022-11-25 05:39:08 -0300

    Tag nsfw/nsfl posts

--- ui.go
+++ ui.go
@@ -228,7 +228,11 @@ func (c *chat) renderMessage(m dggchat.Message) {
 
 	formattedData := m.Message
 	if c.username != "" && strings.Contains(strings.ToLower(m.Message), strings.ToLower(c.username)) || c.isHighlighted(m.Message) {
-		formattedData = fmt.Sprintf("%s%s%s%s", c.config.HighlightBg, c.config.HighlightFg, m.Message, reset) // change message color if you get mentioned or the message contains a highlighed string
+		formattedData = fmt.Sprintf("%s%s%s%s", c.config.HighlightBg, c.config.HighlightFg, m.Message, reset) // change message color if the message contains a highlighed string
+	} else if strings.Contains(m.Message, "nsfl") {
+		formattedData = fmt.Sprintf("%s%s%s", fgBrightYellow, m.Message, reset) // nsfl post
+	} else if strings.Contains(m.Message, "nsfw") {
+		formattedData = fmt.Sprintf("%s%s%s", fgBrightRed, m.Message, reset) // nsfw post
 	} else if strings.HasPrefix(m.Message, ">") {
 		formattedData = fmt.Sprintf("%s%s%s", fgGreen, m.Message, reset) // greentext
 	}
